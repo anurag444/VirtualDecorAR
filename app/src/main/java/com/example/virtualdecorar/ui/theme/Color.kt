c7a815e746a7756b3006a3c3e333bf96038c3644
Anurag Teckchandani <anuragteckchandani@gmail.com>
2024-02-15 01:56:57 -0500

    Initial commit Added Custom colors and fonts

---
+++ app/src/main/java/com/example/virtualdecorar/ui/theme/Color.kt
@@ -0,0 +1,26 @@
+package com.example.virtualdecorar.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Green = Color(0xFF183D3D)
+val LightGreen1 = Color(0xFF93B1A6)
+val LightGreen2 = Color(0xFFA6BAB3)
+
+val White = Color(0xFFF1F1F1)
+val WhiteSHADE1 = Color(0xFFE7E7E7)
+val WhiteSHADE2 = Color(0xFFDFDFDF)
+
+val Black = Color(0xFF0A0B0B)
+val LightBlack1 = Color(0xFF232323)
+val LightBlack2 = Color(0xFF3B3C3C)
+
+val Gray = Color(0xFF9D9D9D)
+val GraySHADE = Color(0xFF6C6D6D)
